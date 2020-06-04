126f5015c97fa87b7d17b98ed9310834a44de916
ss <sssscorejava@gmail.com>
2020-06-05 00:50:52 +0100

    feat: Added slider component of react-native and linked it to metricMetainfo

--- components/FitSlider.js
+++ components/FitSlider.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Slider } from 'react-native';
 
-function FitSlider() {
+function FitSlider(props) {
     return (
         <View>
             <Text> Fit slider</Text>
+            <Slider 
+            minimumValue={0}
+            maximumValue={props.max}
+            step={props.step}
+            value={props.value}
+            onValueChange={props.onChange}
+            />
+            <Text>{props.value}</Text>
+            <Text>{props.unit}</Text>
         </View>
     )
 }
