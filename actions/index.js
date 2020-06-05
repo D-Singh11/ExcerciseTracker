2b7137f6b8014467b553eba974c391ef30f92c7f
ss <sssscorejava@gmail.com>
2020-06-05 16:05:10 +0100

    feat: Created folser for actions and added Action types and actions creator functions for Getting entries from the API to save to redux store.
    And Adding new entry to store

---
+++ actions/index.js
@@ -0,0 +1,17 @@
+export const ADD_ENTRY = 'ADD_ENTRY';
+export const RECEIVE_ENTRIES = 'RECEIVE_ENTRIES';
+
+export function receiveEntries(entries) {
+    return {
+        type: RECEIVE_ENTRIES,
+        entries
+    };
+}
+
+
+export function addEntry(entry) {
+    return {
+        type: ADD_ENTRY,
+        entry
+    };
+}
