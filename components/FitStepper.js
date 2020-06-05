3ba2b78fb65a96dee53953b1e82959270ab9e72c
ss <sssscorejava@gmail.com>
2020-06-05 01:02:34 +0100

    feat:Built FitStepper component using react-native TouchableOpacity and FontAwesome icons and connect it to the metric information whichis passed as props to it

--- components/FitStepper.js
+++ components/FitStepper.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
 
-function FitSlider() {
+function FitSlider(props) {
     return (
         <View>
-            <Text> Fit stepper</Text>
+            <View>
+                <TouchableOpacity onPress={props.onDecrement}>
+                    <FontAwesome name='minus' size={30} color={'black'} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={props.onIncrement}>
+                    <FontAwesome name='plus' size={30} color={'black'} />
+                </TouchableOpacity>
+            </View>
+            <Text>{props.value}</Text>
+            <Text>{props.unit}</Text>
         </View>
     )
 }
