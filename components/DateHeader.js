e30f8e6c3115a32049be40591cefbcadb64d10cb
ss <sssscorejava@gmail.com>
2020-06-06 02:39:05 +0100

    style: applied styling to DateHeader component

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { purple } from '../utils/colors';
 
 export default function DateHeader({date}) {
     return (
-        <Text>
+        <Text style={{color:purple, fontSize:25, paddingTop:13}}>
             {date}
         </Text>
     )
