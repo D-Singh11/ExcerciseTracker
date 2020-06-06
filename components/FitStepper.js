f26169f18e819d524e949e799ae4967434864f48
ss <sssscorejava@gmail.com>
2020-06-06 02:35:42 +0100

    style: Created styles for android buttons for FitStepper component
    And applied conditional/ platform specific styles using the Platform component of 'react-native'

--- components/FitStepper.js
+++ components/FitStepper.js
@@ -7,18 +7,33 @@ import { purple, white, gray } from '../utils/colors';
 function FitSlider(props) {
     return (
         <View style={[styles.row, { justifyContent: 'space-between' }]}>
-            <View style={{ flexDirection: 'row' }}>
-                <TouchableOpacity
-                    style={[styles.iosBtn, { borderTopRightRadius: 0, borderBottomRightRadius: 0 }]}
-                    onPress={props.onDecrement}>
-                    <FontAwesome name='minus' size={30} color={purple} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[styles.iosBtn, , { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }]}
-                    onPress={props.onIncrement}>
-                    <FontAwesome name='plus' size={30} color={purple} />
-                </TouchableOpacity>
-            </View>
+            {Platform.os === 'ios'
+                ? <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity
+                        style={[styles.iosBtn, { borderTopRightRadius: 0, borderBottomRightRadius: 0 }]}
+                        onPress={props.onDecrement}>
+                        <Entypo name='minus' size={30} color={purple} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.iosBtn, , { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }]}
+                        onPress={props.onIncrement}>
+                        <Entypo name='plus' size={30} color={purple} />
+                    </TouchableOpacity>
+                </View>
+                : <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity
+                        style={[styles.androidBtn, { borderTopRightRadius: 0, borderBottomRightRadius: 0 }]}
+                        onPress={props.onDecrement}>
+                        <FontAwesome name='minus' size={30} color={white} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.androidBtn, , { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }]}
+                        onPress={props.onIncrement}>
+                        <FontAwesome name='plus' size={30} color={white} />
+                    </TouchableOpacity>
+                  </View>
+
+            }
             <View style={styles.metricCounter}>
                 <Text style={{ fontSize: 24, textAlign: 'center' }}>{props.value}</Text>
                 <Text style={{ fontSize: 18, color: gray }}>{props.unit}</Text>
@@ -46,6 +61,13 @@ const styles = StyleSheet.create({
         width: 85,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    androidBtn: {
+        margin: 5,
+        backgroundColor: purple,
+        padding: 10,
+        borderRadius: 2
+
     }
 })
 
