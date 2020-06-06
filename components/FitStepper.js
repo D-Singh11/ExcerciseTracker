429e47b173812d099473a6f6c767c2c0efd66b51
ss <sssscorejava@gmail.com>
2020-06-06 02:11:04 +0100

    style Styled the FitStepper Component for ios devices

--- components/FitStepper.js
+++ components/FitStepper.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Platform } from 'react-native';
 import { FontAwesome, Entypo } from '@expo/vector-icons';
+// import { white, green, purple } from 'ansi-colors';
+import { purple, white } from '../utils/colors';
 
 function FitSlider(props) {
     return (
-        <View>
-            <View>
-                <TouchableOpacity onPress={props.onDecrement}>
-                    <FontAwesome name='minus' size={30} color={'black'} />
+        <View style={[styles.row, { justifyContent: 'space-between' }]}>
+            <View style={{ flexDirection: 'row' }}>
+                <TouchableOpacity
+                    style={styles.iosBtn}
+                    onPress={props.onDecrement}>
+                    <FontAwesome name='minus' size={30} color={purple} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={props.onIncrement}>
-                    <FontAwesome name='plus' size={30} color={'black'} />
+                <TouchableOpacity
+                    style={styles.iosBtn}
+                    onPress={props.onIncrement}>
+                    <FontAwesome name='plus' size={30} color={purple} />
                 </TouchableOpacity>
             </View>
             <Text>{props.value}</Text>
@@ -18,5 +24,22 @@ function FitSlider(props) {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    iosBtn: {
+        backgroundColor: white,
+        borderColor: purple,
+        borderWidth: 1,
+        borderRadius: 3,
+        padding: 5,
+        paddingLeft: 25,
+        paddingRight: 25
+    }
+})
 
 export default FitSlider
