04db7a42d3f2645dd409489faa83ebd28ccfee80
ss <sssscorejava@gmail.com>
2020-06-06 02:53:04 +0100

    styles: Applied styling to FitSlider component

--- components/FitSlider.js
+++ components/FitSlider.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import { Text, View, Slider } from 'react-native';
+import React, { Component, Fragment } from 'react';
+import { Text, View, Slider, StyleSheet } from 'react-native';
+import { gray } from '../utils/colors';
 
 function FitSlider(props) {
     return (
-        <View>
-            <Text> Fit slider</Text>
-            <Slider 
-            minimumValue={0}
-            maximumValue={props.max}
-            step={props.step}
-            value={props.value}
-            onValueChange={props.onChange}
+        <View style={[styles.row, { justifyContent: 'space-between' }]}>
+            <Slider
+                minimumValue={0}
+                maximumValue={props.max}
+                step={props.step}
+                value={props.value}
+                onValueChange={props.onChange}
             />
-            <Text>{props.value}</Text>
-            <Text>{props.unit}</Text>
+            <View style={styles.metricCounter}>
+                <Text style={{ fontSize: 25, textAlign: "center" }}>{props.value}</Text>
+                <Text style={{ fontSize: 18, color: gray }}>{props.unit}</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default FitSlider
