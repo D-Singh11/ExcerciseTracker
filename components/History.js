4c2d0a55a3eb539b463b9208f98b34030baf872e
ss <sssscorejava@gmail.com>
2020-06-07 21:49:55 +0100

    feat: Display actual data instead of stringigy version
    Added DateHeader component to show date as header.
    Styled the component.
    Applied touchableopacity button to the day where data was logged.
    For currentday/today if no data is logged then an message to log data is displayed.
    For all other days if there is no data for that day then message saying that you didnt log any data is showed.

--- components/History.js
+++ components/History.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { receiveEntries, addEntry } from '../actions';
 import { timeToString, getDailyReminderValue } from '../utils/helpers';
 import { fetchCalenderResults } from '../utils/api';
 import UdaciFitnessCalendar from 'udacifitness-calendar';
+import { white } from '../utils/colors';
+import DateHeader from './DateHeader';
 
 class History extends Component {
     componentDidMount() {
@@ -15,10 +17,11 @@ class History extends Component {
             if (!entries[timeToString()]) {                         // if no entry/data for today then we
                 const key = timeToString();                          // create a key with today's date and reminder message as value to store in the entries for today's entry
                 this.props.dispatch(addEntry({
-                    [key]: getDailyReminderValue()                      // dispatch action to store message in store
+                    [timeToString()]: getDailyReminderValue()                      // dispatch action to store message in store
                 }))
             }
         })
+        // .then(() => this.setState(() => ({ready: true})))
 
         console.log(this.props);
     }
@@ -30,35 +33,68 @@ class History extends Component {
     // Therefore message stored in side today property is displayed.
     renderItem = ({ today, ...metrics }, formattedDate, key) => {
         return (
-            <View>
+            <View style={styles.item}>
                 {today
-                    ? <Text>{JSON.stringify(today)}</Text>
-                    : <Text>{JSON.stringify(metrics)}</Text>
+                    ? <View>
+                        <DateHeader date={formattedDate} />
+                        <Text style={styles.noDataText}>
+                            {today}
+                        </Text>
+                    </View>
+                    : <TouchableOpacity onPress={() => console.log('Pressed')}>
+                        <Text>{JSON.stringify(metrics)}</Text>
+                    </TouchableOpacity>
                 }
             </View>
         )
     }
 
+    // if there is no data for the day(but not today) then show following view
     renderEmptyDate = (formattedDate) => {
         return (
-            <Text>No data for this day.</Text>
+            <View style={styles.item}>
+                <Text style={styles.noDataText}>
+                    You didn't log any data/add on this day.
+                    </Text>
+            </View>
         )
     }
 
     render() {
         return (
-            <View style={{flex:1}}>
 
-                <UdaciFitnessCalendar
-                    items={this.props.entries}
-                    renderItem={this.renderItem}
-                    renderEmptyDate={this.renderEmptyDate}
-                />
-            </View>
+            <UdaciFitnessCalendar
+                items={this.props.entries}
+                renderItem={this.renderItem}
+                renderEmptyDate={this.renderEmptyDate}
+            />
         )
     }
 }
 
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: white,
+        borderRadius: Platform.os === 'ios' ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+    },
+    noDataText: {
+        fontSize: 20,
+        paddingTop: 20,
+        paddingBottom: 20
+    }
+})
 
 function mapStateToProps(entries) {
     return {
