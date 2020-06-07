4f09762b62c35a20006ba7f7f6b06f18acae1fc8
ss <sssscorejava@gmail.com>
2020-06-07 22:12:21 +0100

    feat: Built new MetricCrad component to show the already logged in data for the day in history tab/component

---
+++ components/MetricCard.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { getMetricMetaInfo } from '../utils/helpers';
+import { gray } from '../utils/colors';
+import DateHeader from './DateHeader';
+
+function MetricCard({ date, metrics }) {
+    return (
+        <View>
+            {date && <DateHeader date={date} />}
+            {Object.keys(metrics).map(metricKey => {
+                const { getIcon, displayName, unit, backgroundColor } = getMetricMetaInfo(metricKey);
+
+                return (
+                    <View style={StyleSheet.metric} key={metricKey}>
+                        {getIcon()}
+                        <View>
+                            <Text style={{ fontSize: 20 }}>
+                                {displayName}
+                            </Text>
+                            <Text style={{ fontSize: 16, color: gray }}>
+                                {metrics[metricKey]} {unit}
+                            </Text>
+                        </View>
+                    </View>
+                )
+            })}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    metric: {
+        flexDirection: 'row',
+        marginTop: 20
+    }
+})
+
+export default MetricCard
