b5cc26f5131ee9c54bb79fd8fd562156fb088a54
ss <sssscorejava@gmail.com>
2020-06-08 18:01:04 +0100

    feat: Connected the EntryDetail component to redux store because we need to get data for the selected day

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import { View, Text, Platform, StatusBar } from 'react-native';
+import { View, Text, Platform, StatusBar, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { white } from '../utils/colors';
 
-export default class EntryDetail extends Component {
+class EntryDetail extends Component {
 
     setTitleasDate = () => {
         const { entryId } = this.props.route.params;               // get entry id from thr route props
 
         // format date/entryId
-        const year = entryId.slice(0,4);
-        const month = entryId.slice(5,7);
+        const year = entryId.slice(0, 4);
+        const month = entryId.slice(5, 7);
         const day = entryId.slice(8);
 
         // any property returned in the following overrides the Stack navigations 'options' props property which were passed when stack navigator was declared
-       this.props.navigation.setOptions({
-            title : `${month}/${day}/${year}`  
+        this.props.navigation.setOptions({
+            title: `${month}/${day}/${year}`
         });
 
     }
@@ -27,3 +29,5 @@ export default class EntryDetail extends Component {
         )
     }
 }
+
+export default connect()(EntryDetail);
