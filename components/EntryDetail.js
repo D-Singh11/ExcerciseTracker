1f252ff85ce199d0584b160b6c8e1cc20ec82ceb
ss <sssscorejava@gmail.com>
2020-06-09 00:29:57 +0100

    feat: Added functionality to reset the entry/data for the selected day by clicking the reset button.
    Updates/ remove the entry from redux store.
    return a message if removed entry/data/day was today, Otherwise return null in metric object.

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -3,10 +3,20 @@ import { View, Text, Platform, StatusBar, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { white } from '../utils/colors';
 import MetricCard from './MetricCard';
+import { addEntry } from '../actions';
+import { removeEntry } from '../utils/api';
+import { timeToString, getDailyReminderValue } from '../utils/helpers';
+import TextButton from './TextButton';
 
 class EntryDetail extends Component {
 
-    setTitleasDate = () => {
+
+    // lifecycle hook used to only update component if metrics are not null and metrics have no 'today' property
+    shouldComponentUpdate(nextProps) {
+        return nextProps.metrics !== null && !nextProps.metrics.today
+    }
+
+    setTitleAsDate = () => {
         const { entryId } = this.props.route.params;               // get entry id from thr route props
 
         // format date/entryId
@@ -18,14 +28,27 @@ class EntryDetail extends Component {
         this.props.navigation.setOptions({
             title: `${month}/${day}/${year}`
         });
-
     }
+
+
+
+    // function used to reset the entry for selected day
+    reset = () => {
+        this.props.remove();            // remove entry from redux store, getting remove from props using mapDispatchToProps function
+        this.props.goBack();            // navigate user to the previous screen
+        removeEntry(this.props.entryId);        // now remove the entry/data linked to entryId/day from the asyncstorage. like removing from api/backend
+    }
+
     render() {
-        this.setTitleasDate();
+        this.setTitleAsDate();
+        console.log(this.props);
         return (
             <View style={styles.container}>
                 <Text> Entry Detail</Text>
                 <MetricCard date={this.props.entryId} metrics={this.props.metrics} />
+                <TextButton onPress={this.reset} style={{ margin: 20 }}>
+                    Reset
+                </TextButton>
             </View>
         )
     }
@@ -34,8 +57,8 @@ class EntryDetail extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        backgroundColor:white,
+        flex: 1,
+        backgroundColor: white,
         padding: 15
     }
 })
@@ -49,4 +72,27 @@ function mapStateToProps(state, props) {
     }
 }
 
-export default connect(mapStateToProps)(EntryDetail);
+
+// this function is used to perform action in the container component instead of 
+// dispatching actions in this component itself (this.props.dispatch). 
+// Following function allow us to move the dispatch calls away from UI component and make them
+// within the Container/Connected component of connect() functions intternal HOC.
+// It returns an object which has two functions remove() which removes the entry from redux store
+// goback() which navigate to the previous view of application.
+// So instead of having all that logic in the component, we put it in mapDispatchToProps 
+// then the UI component can simply call props.remove() or props.goBack
+function mapDispatchToProps(dispatch, props) {
+    const entryId = props.route.params.entryId;
+
+    return {
+        remove: () => dispatch(addEntry({
+            [entryId]: timeToString() === entryId
+                ? getDailyReminderValue()
+                : null
+        })),
+
+        goBack: () => props.navigation.goBack()
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail);
