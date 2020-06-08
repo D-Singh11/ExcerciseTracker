85ca1618807ba12d85e4371f1280bee463c8bfae
ss <sssscorejava@gmail.com>
2020-06-08 02:01:48 +0100

    feat:Added styling to tabs and display platform specific tab using appropriate tab navigation creator
    createMaterialTopTabNavigator() - for android
    createBottomTabNavigator() - for ios

--- App.js
+++ App.js
@@ -13,19 +13,59 @@ import { Ionicons, FontAwesome } from 'react-native-vector-icons';
 
 const store = createStore(reducers);
 
-const Tab = createMaterialTopTabNavigator()
+const RouteConfigs = {
+  History: {
+    name: "History",
+    component: History,
+    options: { tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />, title: 'History' }
+  },
+  AddEntry: {
+    component: AddEntry,
+    name: "Add Entry",
+    options: { tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />, title: 'Add Entry' }
+  }
+}
+
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === "ios" ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === "ios" ? white : purple,
+      shadowColor: "rgba(0, 0, 0, 0.24)",
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+};
+
+const Tab = Platform.OS === "ios"
+  ? createBottomTabNavigator()
+  : createMaterialTopTabNavigator()
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={{flex:1}}>
-          <View style={{height: 20}}>
-            
+        <View style={{ flex: 1 }}>
+          <View style={{ height: 20 }}>
           </View>
           <NavigationContainer>
-            <Tab.Navigator>
-              <Tab.Screen name="Hello" component={History} />
+            {/* Spread all the properties of TabNavigatorConfig to pass them as props */}
+            <Tab.Navigator {...TabNavigatorConfig}>
+
+              {/* one way to specify route configs using spread operator. RouteConfigs is defined as object vsriable */}
+              <Tab.Screen {...RouteConfigs['AddEntry']} />  
+
+                  {/* Other way to specify all props using inline syntax */}
               <Tab.Screen name="Bye" component={AddEntry} />
             </Tab.Navigator>
           </NavigationContainer>
