a3c0a053461ff1b1ecfd4dc72b0694304c1848de
ss <sssscorejava@gmail.com>
2020-06-08 01:46:24 +0100

    feat: Added basic  tabs to app to show History and AddEntry components/views using react navigation v5

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import AddEntry from './components/AddEntry';
 import History from './components/History';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
+import { purple, white, red } from './utils/colors';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons, FontAwesome } from 'react-native-vector-icons';
 
 const store = createStore(reducers);
+
+const Tab = createMaterialTopTabNavigator()
 
 export default class App extends React.Component {
   render() {
@@ -14,10 +21,14 @@ export default class App extends React.Component {
       <Provider store={store}>
         <View style={{flex:1}}>
           <View style={{height: 20}}>
-
+            
           </View>
-          <History />
-          {/* <AddEntry /> */}
+          <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen name="Hello" component={History} />
+              <Tab.Screen name="Bye" component={AddEntry} />
+            </Tab.Navigator>
+          </NavigationContainer>
         </View>
       </Provider>
     );
