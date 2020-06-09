c012db3b89bea6535603fe238c2b3782fa628e14
ss <sssscorejava@gmail.com>
2020-06-09 18:32:18 +0100

    feat: Animated Library practice
    Demo how 'spring' type of 'Animated' library works.
    Example animates height and width of image from 0 to 300 with a speed of 5. We did not use duration because 'Animated.spring' does not have duration property and durationis only applicabe to 'Animated.timing'
    Animated.spring(property: {targetValue, speed}

--- App.js
+++ App.js
@@ -14,19 +14,26 @@ const store = createStore(reducers);
 export default class App extends React.Component {
 
   state = {
-    opacity: new Animated.Value(0)            // create new Animated type property
+    opacity: new Animated.Value(0),            // create new Animated type property with initial value 0f 0
+    width: new Animated.Value(0),
+    height: new Animated.Value(0),
   }
 
   componentDidMount() {
-    const { opacity } = this.state;
+    const { opacity, width, height } = this.state;        // destructuring state
 
     // uses timig type of Animated which is provided by react-navtive
     Animated.timing(opacity, { toValue: 1, duration: 1000 })        // specify that opacity value should go to 1 in 1 second
-    .start()                                                        // start the animation using start() of Animated
+      .start()                                                        // start the animation using start() of Animated
+
+    // uses 'spring' type of Animated which is provided by react-navtive
+    Animated.spring(width, { toValue: 300, speed: 5 }).start()        // specify that width value should go to 300 with speed of 5. we not using duration here because we are using 'spring' type of Animated library
+    Animated.spring(height, { toValue: 300, speed: 5 }).start()        // specify that width value should go to 300 with speed of 5. we not using duration here because we are using 'spring' type of Animated library
+
   }
 
   render() {
-    const { opacity } = this.state;
+    const { opacity, width, height } = this.state;
     return (
       // <Provider store={store}>
       //   <View style={{ flex: 1 }}>
@@ -37,7 +44,7 @@ export default class App extends React.Component {
       //   </View>
       // </Provider>
       <View style={styles.container}>
-        <Animated.Image style={[styles.img, {opacity}]}
+        <Animated.Image style={[styles.img, { opacity, width, height }]}
           source={{ uri: 'https://tylermcginnis.com/tylermcginnis_glasses-300.png' }}
         />
       </View>
